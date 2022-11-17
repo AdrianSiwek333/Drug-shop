3db8438c77eb4216636d3f2820db57d709be12bf
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-17 19:48:43 +0100

    Update product.php

--- product.php
+++ product.php
@@ -4,16 +4,28 @@
 <main>
     <div class="container">
         <div class="row">
+                <?php
+                    $id=$_GET['produkt'];
+                    $stmt=$db_con->query("SELECT * from products where product_id='$id'");
+                    while($row=$stmt->fetch()){  
+                        $cat_id=$row['category_id'];
+                        $stmt1=$db_con->query("SELECT * from categories where category_id='$cat_id'");
+                        while($row1=$stmt1->fetch()){                   
+                ?>
             <div class="col-md-12 col-xl-6">
-                <img class="productImage" src="pictures/m011t167_nasal_spray_01.jpg">
-                <p class="productTitle"> Anty-Katar </p>
+                <img class="productImage" src="<?=$row['image']?>">
+                <p class="productTitle"><?=$row['product_name']?></p>
             </div>
             <div class="col-md-12 col-xl-6">
-                <p class="productDescription">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget venenatis tellus, non pharetra est. Nam at purus id quam feugiat scelerisque non id sem. Donec ac odio orci. Sed elit enim, bibendum nec arcu non, venenatis iaculis velit. Maecenas scelerisque vitae lacus id fringilla. Morbi quis magna pellentesque velit congue eleifend a rutrum arcu. Pellentesque faucibus eget dui non pretium. Mauris condimentum massa at gravida blandit. Donec elit lectus, lobortis a purus vitae, porttitor aliquam libero. Cras rutrum aliquam leo, at ultrices libero blandit vel. Nam nec risus odio. Aliquam condimentum tortor quam, non vehicula felis convallis eu. Nam luctus accumsan porta. Praesent a varius elit, vehicula egestas sem.
-                    Nam tempus velit sit amet arcu congue dictum. Proin tristique vitae ligula ac feugiat. Suspendisse ultrices malesuada ex at consequat. In congue vel elit vulputate imperdiet. Morbi fringilla neque et urna lobortis, a placerat nunc malesuada. Suspendisse eu mattis tortor. Vivamus ultricies mi non augue rutrum porttitor. Duis et rhoncus massa. Fusce porta erat in erat feugiat, eu dignissim nibh lobortis. Sed a venenatis sem, eget porta elit. Aenean nec mauris erat. Phasellus sit amet egestas purus, vitae mattis lorem. Mauris nec tincidunt nisi. Ut condimentum mattis aliquet.
-                </p>
+                <p class="productDescription">Kategoria: <?=$row1['category_name']?></p>
+                <p class="productDescription"><?=$row['description']?></p>
+                <p class="productDescription">cena: <?=$row['price']?>$</p>
+                <p class="productDescription">Ilość w magazynie: <?=$row['quantity']?></p>
             </div>
+            <?php
+                    }
+                }
+            ?>
         </div>
         <div class="row">
             <div class="col-md-12">
