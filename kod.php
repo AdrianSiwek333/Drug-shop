356fd303d5cb7893d857f2f051a7fb6da4de69e4
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-17 20:58:11 +0100

    Update kod.php

--- kod.php
+++ kod.php
@@ -1,60 +1,69 @@
 <?php
+    include('header.php');
+    ?>
+<div class="container-fluid">
+    
+<section class="vh-150  gradient-custom">
+  <div class="container  py-5 h-60">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-light text-dark" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
 
-if(isset($_POST['wyslij']))
-{
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $email = $_POST['email'];
-    $title = $_POST['title'];
-    $text = $_POST['text'];
+            <div class="mb-md-5 mt-md-4 pb-5">
 
-    $query = "INSERT INTO contact(name, surname, email, title, text) VALUES (:name, :surname, :email, :title, :text)";
-    $query_run=$db_con->prepare($query);
+              <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+              
+              <form method="post">
+              <div class="form-outline form-white mb-4">
+                <input type="email" name="email" class="form-control form-control-lg" />
+                <div class="form-label" for="email">Email</div>
+              </div>
+              <div class="form-outline  form-white mb-4">
+                <input type="login" name="login" class="form-control  form-control-lg" />
+                <div class="form-label" for="login">Login</div>
+              </div>
 
-    $data = [
-        ':name'=>$name,
-        ':surname'=>$surname,
-        ':email'=>$email,
-        ':title'=>$title,
-        ':text'=>$text,
-    ];
-    $query_execute = $query_run->execute($data);
+              <div class="form-outline  form-white mb-4">
+                <input type="fname" name="fname" class="form-control  form-control-lg" />
+                <div class="form-label" for="fname">Imię</div>
+              </div>
+              <div class="form-outline  form-white mb-4">
+                <input type="lname" name="lname" class="form-control  form-control-lg" />
+                <div class="form-label" for="lname">Nazwisko</div>
+              </div>
+              
+              
+              <div class="form-outline  form-white mb-4">
+                <input type="password" name="password" class="form-control  form-control-lg" />
+                <div class="form-label" for="password">Hasło</div>
+              </div>
+              <button class="buttonBlue buttonSearch" type="submit" name="register">Zarejestruj</button>
 
-    if($query_execute)
-    {
-        $wyslano='Wysłano';
-    }
-    else{
-        $wyslano='Błąd. Spróbuj ponownie.';
-    }
-}
+              </form>
 
-if(isset($_POST['register']))
-{
-    $email = $_POST['email'];
-    $login = $_POST['login'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $password = $_POST['password'];
 
-    $query = "INSERT INTO users(email, login, fname, lname, password) VALUES (:email, :login, :fname, :lname, :password)";
-    $query_run=$db_con->prepare($query);
+              <p class="mb-0">Masz konto? <a href="login.php" class="text-dark fw-bold">Zaloguj się</a>
+                
+                  <?php if(isset($_POST['register'])){
+                echo "<h1>" . $rejestracja . "</h1>";
+                  }
+                ?>
+            </div>
 
-    $data = [
-        ':email'=>$email,
-        ':login'=>$login,
-        ':fname'=>$fname,
-        ':lname'=>$lname,
-        ':password'=>$password,
-    ];
-    $query_execute = $query_run->execute($data);
+            <div>
+              
+              </p>
+            </div>
 
-    if($query_execute)
-    {
-        $rejestracja='Zarejestrowano';
-    }
-    else{
-        $rejestracja='Błąd. Spróbuj ponownie.';
-    }
-}
-?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+<?php
+    include('footer.php');
+    ?>
