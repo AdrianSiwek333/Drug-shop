2fe10c45579e2d71f725c294b9d5e1bd4f845bf4
TchorzuD <104624920+TchorzuD@users.noreply.github.com>
2022-11-17 14:40:26 +0100

    Update nasze-produkty.php

--- nasze-produkty.php
+++ nasze-produkty.php
@@ -13,44 +13,51 @@
                 <div class="col-md-3">
                     <div class="filter">
                         <div class="sidebar">
-                            <p>Kategorie</p>
-                            <input type="radio" id="bolbrzucha" value="0" name="kategorie">
-                            <label for="bolbrzucha"><span>Ból brzucha</span></label>
-                            <input type="radio" id="bolglowy" value="1" name="kategorie">
-                            <label for="bolglowy"><span>Ból głowy</span></label>
-                            <input type="radio" id="kaszel" value="2" name="kategorie">
-                            <label for="kaszel"><span>Kaszel</span></label>
-                            <input type="radio" id="katar" value="3" name="kategorie">
-                            <label for="katar"><span>Katar</span></label>
-                            <p>Pojemność</p>
-                            <input type="radio" id="50ml" value="4" name="pojemnosc">
-                            <label for="50ml"><span>50 ml</span></label>
-                            <input type="radio" id="100ml" value="5" name="pojemnosc">
-                            <label for="100ml"><span>100 ml</span></label>
-                            <input type="radio" id="12tabletek" value="6" name="pojemnosc">
-                            <label for="12tabletek"><span>12 tabletek</span></label>
-                            <input type="radio" id="24tabletki" value="7" name="pojemnosc">
-                            <label for="24tabletki"><span>24 tabletki</span></label>
-                            <p>Typ</p>
-                            <input type="radio" id="plyn" value="8" name="typ">
-                            <label for="plyn"><span>Płyn</span></label>
-                            <input type="radio" id="spray" value="9" name="typ">
-                            <label for="spray"><span>Spray</span></label>
-                            <input type="radio" id="tabletki" value="10" name="typ">
-                            <label for="tabletki"><span>Tabletki</span></label>
-                            <input type="radio" id="tabletkiDoSsania" value="11" name="typ">
-                            <label for="tabletkiDoSsania"><span>Tabletki do ssania</span></label>
-                            <input class="buttonBlue buttonSearch" type="submit" value="Szukaj">
-                            <input class="buttonClear" type="submit" value="Wyczyść filtr">
+                            <form method="POST" action="nasze-produkty.php">
+                                <input type="text" name="search" class="searchInput">
+                                <p>Kategorie</p>
+                                <input type="radio" id="bolbrzucha" value="0" name="kategorie">
+                                <label for="bolbrzucha"><span>Ból brzucha</span></label>
+                                <input type="radio" id="bolglowy" value="1" name="kategorie">
+                                <label for="bolglowy"><span>Ból głowy</span></label>
+                                <input type="radio" id="kaszel" value="2" name="kategorie">
+                                <label for="kaszel"><span>Kaszel</span></label>
+                                <input type="radio" id="katar" value="3" name="kategorie">
+                                <label for="katar"><span>Katar</span></label>
+                                <p>Pojemność</p>
+                                <input type="radio" id="50ml" value="4" name="pojemnosc">
+                                <label for="50ml"><span>50 ml</span></label>
+                                <input type="radio" id="100ml" value="5" name="pojemnosc">
+                                <label for="100ml"><span>100 ml</span></label>
+                                <input type="radio" id="12tabletek" value="6" name="pojemnosc">
+                                <label for="12tabletek"><span>12 tabletek</span></label>
+                                <input type="radio" id="24tabletki" value="7" name="pojemnosc">
+                                <label for="24tabletki"><span>24 tabletki</span></label>
+                                <p>Typ</p>
+                                <input type="radio" id="plyn" value="8" name="typ">
+                                <label for="plyn"><span>Płyn</span></label>
+                                <input type="radio" id="spray" value="9" name="typ">
+                                <label for="spray"><span>Spray</span></label>
+                                <input type="radio" id="tabletki" value="10" name="typ">
+                                <label for="tabletki"><span>Tabletki</span></label>
+                                <input type="radio" id="tabletkiDoSsania" value="11" name="typ">
+                                <label for="tabletkiDoSsania"><span>Tabletki do ssania</span></label>
+                                <input class="buttonClear" type="submit" value="Wyczyść filtr">
+                                <button class="buttonBlue buttonSearch" type="submit">Szukaj</button>
+                            </form>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-9">
                     <div class="row">
                         <?php
-                            $stmt = $db_con->query("SELECT * FROM products");
+                            if(!isset($_POST['search'])){      
+                                $stmt = $db_con->query("SELECT * FROM products");
+                            }else{
+                                $stmt = $db_con->query("SELECT * FROM products WHERE product_name like '".$_POST['search']."%'");
+                            }
                             while ($row = $stmt->fetch()) {
-                                echo "<div class='col-md-12 col-lg-6 col-xxl-4 active'>";
+                                echo "<div class='col-md-12 col-lg-6 col-xxl-4 productsShowcase active'>";
                                 echo "<div class='imgShowcase'>";
                                 echo "<img src='".$row['image']."' class='img-fluid' alt='Zdjęcie produktu'>";
                                 echo "</div>";
@@ -58,16 +65,6 @@
                                 echo '<a class="buttonBlue" href="product.php">Sprawdź</a>';
                                 echo "</div>";                            
                             }
-                            /*
-                            
-                            <div class="col-md-12 col-lg-6 col-xxl-4 active">
-                                <div class="imgShowcase">
-                                    <img src="produkt1.jpg" alt="Zdjęcie produktu">
-                                </div>
-                                <h4>Anty-Katar</h4>
-                                <a class="buttonBlue" href="product.php">Sprawdź</a>
-                            </div>
-                            */
                             ?>
 
                     </div>
