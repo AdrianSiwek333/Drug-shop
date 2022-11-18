c0ace765c56d4ed2c97411a24ad37ae0d09768db
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-18 09:36:22 +0100

    Update login.php

--- login.php
+++ login.php
@@ -13,25 +13,27 @@
 
               <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
               <p class="text-white-50 mb-5">Please enter your login and password!</p>
-
+              <form method="post">
               <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <div class="form-label" for="typeEmailX">Email</div>
+                <input type="email" name="email" class="form-control form-control-lg" />
+                <div class="form-label" for="email">Email</div>
               </div>
 
               <div class="form-outline  form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control  form-control-lg" />
-                <div class="form-label" for="typePasswordX">Password</div>
+                <input type="password" name="password" class="form-control  form-control-lg" />
+                <div class="form-label" for="password">Password</div>
               </div>
 
-              <button class="buttonBlue buttonSearch" type="submit">Login</button>
+              <button class="buttonBlue buttonSearch" name="login" type="submit">Login</button>
+            </form>
+            <h1><?=$napis?></h1>
 
            
 
             </div>
 
             <div>
-              <p class="mb-0">Don't have an account? <a href="#!" class="text-dark fw-bold">Sign Up</a>
+              <p class="mb-0">Don't have an account? <a href="register.php" class="text-dark fw-bold">Sign Up</a>
               </p>
             </div>
 
