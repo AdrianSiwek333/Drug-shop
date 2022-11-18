4849da875023bb6288ebc1b61189ff6193cb322e
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-18 09:35:33 +0100

    Update kod.php

--- kod.php
+++ kod.php
@@ -1,69 +1,89 @@
 <?php
-    include('header.php');
-    ?>
-<div class="container-fluid">
+
+if(isset($_POST['wyslij']))
+{
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $title = $_POST['title'];
+    $text = $_POST['text'];
+
+    $query = "INSERT INTO contact(name, surname, email, title, text) VALUES (:name, :surname, :email, :title, :text)";
+    $query_run=$db_con->prepare($query);
+
+    $data = [
+        ':name'=>$name,
+        ':surname'=>$surname,
+        ':email'=>$email,
+        ':title'=>$title,
+        ':text'=>$text,
+    ];
+    $query_execute = $query_run->execute($data);
+
+    if($query_execute)
+    {
+        $wyslano='Wysłano';
+    }
+    else{
+        $wyslano='Błąd. Spróbuj ponownie.';
+    }
+}
+
+if(isset($_POST['register']))
+{
+    $email = $_POST['email'];
+    $login = $_POST['login'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $password = $_POST['password'];
+
+    $hashPassword = password_hash($password,PASSWORD_BCRYPT);
+    $query = "INSERT INTO users(email, login, fname, lname, password) VALUES (:email, :login, :fname, :lname, :password)";
+    $query_run=$db_con->prepare($query);
+
+    $data = [
+        ':email'=>$email,
+        ':login'=>$login,
+        ':fname'=>$fname,
+        ':lname'=>$lname,
+        ':password'=>$password,
+    ];
+    $query_execute = $query_run->execute($data);
+
+    if($query_execute)
+    {
+        $rejestracja='Zarejestrowano';
+    }
+    else{
+        $rejestracja='Błąd. Spróbuj ponownie.';
+    }
+}
+
+    if(isset($_POST['login']))
+    {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $sth = $db_con->prepare('SELECT * FROM users WHERE email=:login limit 1');
+    $sth->bindValue(':login', $email, PDO::PARAM_STR);
+    $sth->execute();
+    $user = $sth->fetch(PDO::FETCH_ASSOC);
+    if($user)
+    {
+    if($password==$user['password'])
+    {
+    $napis =  "Uzytkownik zalogowany pomyslnie";
+
+    }
+    else{
+        $napis= "bledne dane";
+    }
+    }
+    else{
+        $napis= "";
+    }
+    }
+    else{
+    $napis="";
+    }
     
-<section class="vh-150  gradient-custom">
-  <div class="container  py-5 h-60">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-light text-dark" style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
-
-            <div class="mb-md-5 mt-md-4 pb-5">
-
-              <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
-              
-              <form method="post">
-              <div class="form-outline form-white mb-4">
-                <input type="email" name="email" class="form-control form-control-lg" />
-                <div class="form-label" for="email">Email</div>
-              </div>
-              <div class="form-outline  form-white mb-4">
-                <input type="login" name="login" class="form-control  form-control-lg" />
-                <div class="form-label" for="login">Login</div>
-              </div>
-
-              <div class="form-outline  form-white mb-4">
-                <input type="fname" name="fname" class="form-control  form-control-lg" />
-                <div class="form-label" for="fname">Imię</div>
-              </div>
-              <div class="form-outline  form-white mb-4">
-                <input type="lname" name="lname" class="form-control  form-control-lg" />
-                <div class="form-label" for="lname">Nazwisko</div>
-              </div>
-              
-              
-              <div class="form-outline  form-white mb-4">
-                <input type="password" name="password" class="form-control  form-control-lg" />
-                <div class="form-label" for="password">Hasło</div>
-              </div>
-              <button class="buttonBlue buttonSearch" type="submit" name="register">Zarejestruj</button>
-
-              </form>
-
-
-              <p class="mb-0">Masz konto? <a href="login.php" class="text-dark fw-bold">Zaloguj się</a>
-                
-                  <?php if(isset($_POST['register'])){
-                echo "<h1>" . $rejestracja . "</h1>";
-                  }
-                ?>
-            </div>
-
-            <div>
-              
-              </p>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-</div>
-<?php
-    include('footer.php');
-    ?>
+?>
