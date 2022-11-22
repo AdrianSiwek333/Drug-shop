3b56813965cbf64fb041ccddc524c2918ed81bfa
mlonstereck <118387095+mlonstereck@users.noreply.github.com>
2022-11-22 14:00:01 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -3,55 +3,27 @@
     session_start();
     ?>
       <div class="col-md-12">
-          <h5 class="text-center">Cart Items</h5>
+       
           <?php 
           $Total=0;
-          if(isset($_SESSION['cart'])) { ?>
-          <table class="table table-bordered">
-              <thead>
-                 <th>SN</th>
-                 <th>Product</th>
-                 <th>Quantity</th>
-                 <th>Price</th>
-                 <th>Options</th>
-              </thead>
-              <tbody>
-                <?php
-                $index =0;
-                $i=1;
-                foreach($_SESSION['cart'] as $key => $val) {   
-                  $totalPrice = $val['quantity'] * $val['price'];
-                  $Total = $Total + $totalPrice;
-                  ?>            
-                <tr>
-                   <td><?=$i++?></td> 
-                   <td><?=$val['product_name']?></td> 
-                   <td><?=$val['quantity']?></td>  
-                   <td><?=$totalPrice?></td> 
-                   <td><a href="actions.php?action_type=remove_item&index=<?=$key?>">Remove </a></td>
-                </tr>
-              <?php $index++; } ?>
-              <tr>
-                <td></td>
-                <td></td>
-                <td><b>Total</b></td>
-                <td><?=$Total?></td>
-                <td></td>
-              </tr>
-              </tbody>
-
-          </table>
-        <?php } ?>
+?>
         </div>
+        
 <section class="h-100 gradient-custom">
   <div class="container py-5">
     <div class="row d-flex justify-content-center my-4">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <div class="card mb-4">
           <div class="card-header py-3">
-            <h5 class="mb-0">Cart - 2 items</h5>
+            <h5 class="mb-0">Cart</h5>
           </div>
+          <?php
+          foreach($_SESSION['cart'] as $key => $val) { 
+            $totalPrice = $val['quantity'] * $val['price'];
+            $Total = $Total + $totalPrice;
+            ?>   
           <div class="card-body">
+                  
             <!-- Single item -->
             <div class="row">
              
@@ -61,42 +33,36 @@
               <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
                 <!-- Image -->
                 <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
-                    class="w-100" />
-                  <a href="#!">
-                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
-                  </a>
+                
+                    <img class="w-100" src="<?=$val['image']?>">
+                
                 </div>
                 <!-- Image -->
               </div>
 
               <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
                 <!-- Data -->
-                <p><strong>Red hoodie</strong></p>
-                <p>Color: red</p>
-                <p>Size: M</p>
+                <p><strong><?=$val['product_name']?></strong></p>
+                <p>Ilość: <?=$val['quantity']?></p>
+                <p></p>
 
-                <button type="button" class="btn btn-primary btn-sm me-1 mb-2" data-mdb-toggle="tooltip"
-                  title="Remove item">
-                  <i class="fas fa-trash"></i>
-                </button>
-                <button type="button" class="btn btn-danger btn-sm mb-2" data-mdb-toggle="tooltip"
-                  title="Move to the wish list">
-                  <i class="fas fa-heart"></i>
-                </button>
+              
+                <a type="button" href="actions.php?action_type=remove_item&index=<?=$key?>" class="btn btn-danger btn-md mb-2" data-mdb-toggle="tooltip">
+                  Usuń
+               
+                 </a>
                 <!-- Data -->
               </div>
 
               <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 <!-- Quantity -->
-                <div class="d-flex mb-2" style="max-width: 300px">
+                <div class="d-flex mb-3" style="max-width: 300px">
+                
                   <button class="buttonAdd">
                   -
                   </button>
-                  <div class="form-outline">
-                    <input id="form1" min="0" name="quantity" value="1" type="number" class="form-control" />
-                   
-                  </div>
+                
+                    <input id="form1" min="0"  name="quantity" value="<?=$val['quantity']?>" type="number" class="form-control mb-4"  readonly />
                   <button class=" buttonAdd">
                     +
                   </button>
@@ -105,7 +71,7 @@
 
                 <!-- Price -->
                 <p class="text-start text-md-center">
-                  <strong>$17.99</strong>
+                  <strong>Price: <?=$val['price']?> $</strong>
                 </p>
                 <!-- Price -->
               </div>
@@ -113,14 +79,12 @@
             <!-- Single item -->
           </div>
         </div>
-        <div class="card mb-4">
-          <div class="card-body">
-            <p><strong>Expected shipping delivery</strong></p>
-            <p class="mb-0">12.10.2020 - 14.10.2020</p>
-          </div>
-        </div>
+        <?php
+          }
+          ?>
         
       </div>
+      
       <div class="col-md-12">
         <div class="card mb-4">
           <div class="card-header py-3">
@@ -128,11 +92,7 @@
           </div>
           <div class="card-body">
             <ul class="list-group list-group-flush">
-              <li
-                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Products
-                <span>$53.98</span>
-              </li>
+            
               <li class="list-group-item d-flex justify-content-between align-items-center px-0">
                 Shipping
                 <span>Gratis</span>
@@ -145,7 +105,7 @@
                     <p class="mb-0">(including VAT)</p>
                   </strong>
                 </div>
-                <span><strong>$53.98</strong></span>
+                <span><strong><?=$Total?> $</strong></span>
               </li>
             </ul>
 
@@ -155,7 +115,9 @@
           </div>
         </div>
       </div>
+      
     </div>
+  
   </div>
 
 </section>
