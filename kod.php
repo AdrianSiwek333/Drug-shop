fb95fc4b1870babdba241f6bd904ca006de0f342
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-30 14:10:41 +0100

    Update kod.php

--- kod.php
+++ kod.php
@@ -1,84 +1,143 @@
 <?php
+include('header.php');
+?>
+<div class="col-md-12">
 
-if (isset($_POST["produkt"])) {
+  <?php
+  $Total = 0;
+  ?>
+</div>
 
-    setcookie("produkt", $_POST['produkt'], time() + (3600));
-    echo "<script>window.location = 'product.php';</script>";
-}
+<section class="h-100 gradient-custom">
 
-if (isset($_POST['wyslij'])) {
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $email = $_POST['email'];
-    $title = $_POST['title'];
-    $text = $_POST['text'];
+  <?php
+  if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
 
-    $query = "INSERT INTO contact(name, surname, email, title, text) VALUES (:name, :surname, :email, :title, :text)";
-    $query_run = $db_con->prepare($query);
+  ?>
+  <div class="container py-5">
+    <div class="row d-flex justify-content-center my-4">
+      <div class="col-md-12">
+        <div class="card mb-4">
+          <div class="card-header py-3">
+            <h5 class="mb-0">Cart</h5>
+          </div>
+          <?php
+    if (isset($_SESSION['cart'])) {
+      foreach ($_SESSION['cart'] as $key => $val) {
+        $totalPrice = $val['quantity'] * $val['price'];
+        $Total = $Total + $totalPrice;
+          ?>
+          <div class="card-body">
 
-    $data = [
-        ':name' => $name,
-        ':surname' => $surname,
-        ':email' => $email,
-        ':title' => $title,
-        ':text' => $text,
-    ];
-    $query_execute = $query_run->execute($data);
+            <!-- Single item -->
+            <div class="row">
 
-    if ($query_execute) {
-        $wyslano = 'Wysłano';
-    } else {
-        $wyslano = 'Błąd. Spróbuj ponownie.';
+
+              <!-- Single item -->
+              <div class="row">
+                <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                  <!-- Image -->
+                  <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
+
+                    <img class="w-100" src="<?= $val['image'] ?>">
+
+                  </div>
+                  <!-- Image -->
+                </div>
+
+                <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                  <!-- Data -->
+                  <p><strong>
+                      <?= $val['product_name'] ?>
+                    </strong></p>
+                  <p>Ilość: <?= $val['quantity'] ?>
+                  </p>
+                  <p></p>
+
+
+                  <a type="button" href="actions.php?action_type=remove_item&index=<?= $key ?>"
+                    class="btn btn-danger btn-md mb-2" data-mdb-toggle="tooltip">
+                    Usuń
+
+                  </a>
+                  <!-- Data -->
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                  <!-- Quantity -->
+                  <div class="d-flex mb-3" style="max-width: 300px">
+
+                    <button class="buttonAdd">
+                      -
+                    </button>
+
+                    <input id="form1" min="0" name="quantity" value="<?= $val['quantity'] ?>" type="number"
+                      class="form-control mb-4" readonly />
+                    <button class=" buttonAdd">
+                      +
+                    </button>
+                  </div>
+                  <!-- Quantity -->
+
+                  <!-- Price -->
+                  <p class="text-start text-md-center">
+                    <strong>Price: <?= $val['price'] ?> $</strong>
+                  </p>
+                  <!-- Price -->
+                </div>
+              </div>
+              <!-- Single item -->
+            </div>
+          </div>
+          <?php
+      }
     }
-}
+          ?>
 
-if (isset($_POST['register'])) {
-    $email = $_POST['email'];
-    $login = $_POST['login'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $password = $_POST['password'];
+        </div>
 
-    $hashPassword = password_hash($password, PASSWORD_BCRYPT);
-    $query = "INSERT INTO users(email, login, fname, lname, password) VALUES (:email, :login, :fname, :lname, :password)";
-    $query_run = $db_con->prepare($query);
+        <div class="col-md-12">
+          <div class="card mb-4">
+            <div class="card-header py-3">
+              <h5 class="mb-0">Summary</h5>
+            </div>
+            <div class="card-body">
+              <ul class="list-group list-group-flush">
 
-    $data = [
-        ':email' => $email,
-        ':login' => $login,
-        ':fname' => $fname,
-        ':lname' => $lname,
-        ':password' => $hashPassword,
-    ];
-    $query_execute = $query_run->execute($data);
+                <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                  Shipping
+                  <span>Gratis</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                  <div>
+                    <strong>Total amount</strong>
+                    <strong>
+                      <p class="mb-0">(including VAT)</p>
+                    </strong>
+                  </div>
+                  <span><strong>
+                      <?= $Total ?> $
+                    </strong></span>
+                </li>
+              </ul>
 
-    if ($query_execute) {
-        $rejestracja = 'Zarejestrowano';
-    } else {
-        $rejestracja = 'Błąd. Spróbuj ponownie.';
-    }
-}
+              <button type="button" class="buttonBlue buttonSearch">
+                Go to checkout
+              </button>
+            </div>
+          </div>
+        </div>
 
-if (isset($_POST['login'])) {
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-    $sth = $db_con->prepare('SELECT * FROM users WHERE email=:login limit 1');
-    $sth->bindValue(':login', $email, PDO::PARAM_STR);
-    $sth->execute();
-    $user = $sth->fetch(PDO::FETCH_ASSOC);
-    if ($user) {
-        if (password_verify($password, $user['password'])) {
-            setcookie("login", 1, time() + 86400);
-            header("location:index.php");
+      </div>
 
-        } else {
-            $napis = "bledne dane";
-        }
-    } else {
-        $napis = "";
-    }
-} else {
-    $napis = "";
-}
+    </div>
+    <?php
+  } else {
+    echo '<a href="login.php">Jak chcesz kupic zaloguj</a>';
+  }
+    ?>
 
+</section>
+<?php
+include('footer.php');
 ?>
