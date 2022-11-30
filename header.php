52eea29e4f3ec68ed4bd588c1d65d329ef56d003
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-11-30 13:57:08 +0100

    Update header.php

--- header.php
+++ header.php
@@ -23,12 +23,16 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery-migrate@3.4.0/dist/jquery-migrate.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+        </script>
     <script type="text/javascript" src="slick/slick.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
     <?php
     include('db_con.php');
     include('kod.php');
+    if (isset($_COOKIE['login'])) {
+        session_start();
+        $_SESSION['login'] = 1;
+    }
     ?>
 </head>
 
