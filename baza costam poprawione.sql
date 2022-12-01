fc9299ef95882a99cdc9cac2d4af234277529907
asswisniu <118447399+asswisniu@users.noreply.github.com>
2022-12-01 18:13:57 +0100

    Add files via upload

--- baza costam poprawione.sql
+++ baza costam poprawione.sql
@@ -11,29 +11,18 @@ CREATE TABLE products(
     product_name varchar(60) not null,
     description varchar(250),
     PRIMARY key(product_id)
-    FOREIGN key (category_id) REFERENCES categories(category_id)
     );
     
     CREATE TABLE users(
     user_id int AUTO_INCREMENT not null,
     email varchar(60) not null,
-    login varchar(60) not null,
     password varchar(70) not null,
-    fname varchar(60) not null,
-    lname varchar(60) not null,
-    user_type varchar(70) default 'noob',
-    PRIMARY key(user_id)
-    );
-    
-CREATE TABLE customers(
-    customer_id int AUTO_INCREMENT not null,
-    user_id int not null,
     fname varchar(60) not null,
     lname varchar(60) not null,
     town varchar(60) not null,
     address varchar(60) not null,
-    PRIMARY key(customer_id)
-    FOREIGN key (user_id) REFERENCES users(user_id)
+    user_type varchar(70) default 'noob',
+    PRIMARY key(user_id)
     );
     
 CREATE TABLE categories(
@@ -46,31 +35,53 @@ CREATE TABLE categories(
 CREATE TABLE orders(
     order_id int AUTO_INCREMENT not null,
     product_id int not null,
-    customer_id int not null,
+    user_id int not null,
+    order_quantity int unsigned not null,
     order_date date not null,
     PRIMARY key(order_id)
-    FOREIGN key (product_id) REFERENCES products(product_id)
-    FOREIGN key (customer_id) REFERENCES customers(customer_id)
     );
 
-    CREATE TABLE posts(
-        post_id int AUTO_INCREMENT not null,
-        text text(1000) not null,
-        image blob not null,
-        title varchar(500) not null,
-        type varchar(70) not null,
-        PRIMARY KEY(post_id)
+CREATE TABLE posts(
+    post_id int AUTO_INCREMENT not null,
+    text text(1000) not null,
+    image blob not null,
+    title varchar(500) not null,
+    type varchar(70) not null,
+    PRIMARY KEY(post_id)
     );
 
-        CREATE TABLE contact(
-        contact_id int AUTO_INCREMENT not null,
-        text text(1000) not null,
-        title varchar(500) not null,
-        name varchar(70) not null,
-        surname varchar(70) not null,
-        email varchar(70) not null,
-        PRIMARY KEY(contact_id)
+CREATE TABLE contact(
+    contact_id int AUTO_INCREMENT not null,
+    text text(1000) not null,
+    title varchar(500) not null,
+    name varchar(70) not null,
+    surname varchar(70) not null,
+    email varchar(70) not null,
+    PRIMARY KEY(contact_id)
     );
+
+CREATE TABLE invoice(
+    invoice_id int AUTO_INCREMENT not null,
+    order_id int not null,
+    user_id int not null,
+    total_price double (7,2),
+    fname varchar(60) not null,
+    lname varchar(60) not null,
+    town varchar(60) not null, 
+    address varchar(60) not null,
+    PRIMARY KEY(invoice_id)
+);
+
+ALTER TABLE `orders` ADD INDEX(`user_id`);
+ALTER TABLE `orders` ADD FOREIGN KEY (user_id) REFERENCES `users`(user_id);
+ALTER TABLE `orders` ADD INDEX(`product_id`);
+ALTER TABLE `orders` ADD FOREIGN KEY (user_id) REFERENCES `products`(product_id);
+ALTER TABLE `products` ADD INDEX(`category_id`);
+ALTER TABLE `products` ADD FOREIGN KEY (`category_id`) REFERENCES `categories`(category_id);
+ALTER TABLE `invoice` ADD INDEX(`order_id`);
+ALTER TABLE `invoice` ADD FOREIGN KEY (`order_id`) REFERENCES `orders`(order_id);
+ALTER TABLE `invoice` ADD INDEX(`user_id`);
+ALTER TABLE `invoice` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(user_id);
   
 INSERT INTO categories(category_name, description) VALUES
 ('Piguły', 'Bardzo smaczne'),
@@ -96,20 +107,12 @@ INSERT INTO products(product_name, price, quantity, category_id, image, descript
 ('Kociołex', 42.01, 19, 4, 'pictures/koxiolex.jpg', 'Leczy uzależnienia'),
 ('Zuplement', 349.11, 3, 5, 'pictures/zuplement.jpg', 'Nadaje się do stosowania codziennie u dorosłych, nieodpowiedni dla dzieci.');
 
-INSERT INTO customers (fname, lname, town, address, user_id) VALUES
-('Dawid', 'Tchorzewski', 'Zbuczyn', 'Ulica 66', 1),
-('Michał', 'Wisniewski', 'Lukow', 'Ulica 55', 2),
-('Domińk', 'Mlonek', 'Sokołów Podlaski' ,'Ulica 44', 3),
-('AdrJan', 'Siwek', 'Sopot', 'Łosicka 1',4),
-('Lila', 'Jacht', 'Gdańsk', 'Wock 2', 5);
-
-INSERT INTO users (email, password) VALUES
-('dt1@m.pl','Dawid1'),
-('mw2@m.pl','Michal2'),
-('dm3@m.pl','Domink3'),
-('as4@m.pl','Adrjan4'),
-('lj5@m.pl','Lila5')
-;
+INSERT INTO users (user_id, email, password, fname, lname, town, address) VALUES
+(1, 'dt1@m.pl','Dawid1','Dawid', 'Tchorzewski', 'Zbuczyn', 'Ulica 66'),
+(2, 'mw2@m.pl','Michal2','Michal', 'Wisniewski', 'Łuków', 'Ulica 55'),
+(3, 'dm3@m.pl','Domink3','Domińk', 'Mlonek', 'Sokołów Podlaski' ,'Ulica 44'),
+(4, 'as4@m.pl','Adrjan4','AdrJan', 'Siwek', 'Sopot', 'Łosicka 1'),
+(5, 'lj5@m.pl','Lila5','Lila', 'Jacht', 'Gdańsk', 'Wock 2');
 
 INSERT INTO posts (title, text, image, type) VALUES
 ('post1','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'pictures/produkt1.jpg', 'main'),
