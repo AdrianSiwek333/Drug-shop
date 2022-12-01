f0562ed4b67d0a2b0bce15c5e6151281aa1fe374
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-12-01 10:23:10 +0100

    Update nasze-produkty.php

--- nasze-produkty.php
+++ nasze-produkty.php
@@ -79,8 +79,9 @@
                                 <h4><?=$row['product_name']?> <button class="btn fav1 btn-rounded mr-1" data-toggle="tooltip" title="" data-original-title="Add to Fav">
                                        <i class="bi bi-heart"></i> 
                                  </button></h4>
+                                 <p style="text-align: center;"><?=$row['price']?>$ </p>
                                 <form method="post">
-                                <button type="submit" name="produkt" value="<?=$row['product_id']?>" class="buttonBlue">Sprawdz </button>
+                                <button type="submit" name="produkt" value="<?=$row['product_id']?>" class="buttonBlue">Sprawdź</button>
                                 
                             </form>
                                 </div>
