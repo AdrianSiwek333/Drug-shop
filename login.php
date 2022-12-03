ec1a895b90e84bbc7a35001f944d9adf4f162d4a
mlonstereck <118387095+mlonstereck@users.noreply.github.com>
2022-12-03 16:18:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     include('header.php');
     ?>
 <main>
-<section class="vh-100  gradient-custom">
+<section class="vh-150  gradient-custom">
   <div class="container  py-5 h-60">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -11,8 +11,8 @@
 
             <div class="mb-md-5 mt-md-4 pb-5">
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+              <h2 class="fw-bold mb-2 text-uppercase">Zaloguj</h2>
+              <p class="text-black-50 mb-4">Wprowadź dane do logowania</p>
               <form method="post">
               <div class="form-outline form-white mb-4">
                 <input type="email" name="email" class="form-control form-control-lg" />
@@ -21,10 +21,10 @@
 
               <div class="form-outline  form-white mb-4">
                 <input type="password" name="password" class="form-control  form-control-lg" />
-                <div class="form-label" for="password">Password</div>
+                <div class="form-label" for="password">Hasło</div>
               </div>
 
-              <button class="buttonBlue buttonSearch" name="login" type="submit">Login</button>
+              <button class="buttonBlue buttonSearch" name="login" type="submit">Zaloguj</button>
             </form>
             <h1><?=$napis?></h1>
 
@@ -33,7 +33,7 @@
             </div>
 
             <div>
-              <p class="mb-0">Don't have an account? <a href="register.php" class="text-dark fw-bold">Sign Up</a>
+              <p class="mb-0">Nie posiadasz konta? <a href="register.php" class="text-dark fw-bold">Zarejestruj się!</a>
               </p>
             </div>
 
@@ -43,7 +43,7 @@
     </div>
   </div>
 </section>
-    </main>
+</main>
 <?php
     include('footer.php');
     ?>
