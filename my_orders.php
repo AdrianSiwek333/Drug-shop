869fbbdd31d2ca13d7e649905ea2dd0172f1efd1
mlonstereck <118387095+mlonstereck@users.noreply.github.com>
2022-12-11 19:35:18 +0100

    Update my_orders.php

--- my_orders.php
+++ my_orders.php
@@ -1,9 +1,52 @@
 <?php
-include ('header.php');
-$orders_stmt="SELECT * FROM products WHERE product_id IN (SELECT product_id FROM order_info WHERE order_id IN (SELECT order_id FROM orders WHERE user_id LIKE ".$_SESSION['user_idx']." ))";
-$orders=$db_con->query("SELECT * FROM products WHERE product_id IN (SELECT product_id FROM order_info WHERE order_id IN (SELECT order_id FROM orders WHERE user_id LIKE ".$_SESSION['user_idx']." ))");
-while ($order=$orders->fetch()){
-    echo "<img src='".$order['image']."' alt='Zdjęcie produktu'>";
-    echo "<a href='".$order['image']."' download>Pobierz zdjęcie</a>";
-}
-include ('footer.php');
+include('header.php'); ?>
+<div class="container">
+    <main>
+        <style>
+            table{
+                margin-top: 20px;
+            }
+            img {
+                max-width: 400px;
+                max-height: 300px;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+
+
+            }
+
+            .btn {
+                margin-top: 30%;
+                margin-left: auto;
+                margin-right: auto;
+               
+
+            }
+        </style>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Nazwa produktu</th>
+                    <th scope="col">Zdjęcie poglądowe</th>
+                    <th scope="col">Pobieranie</th>
+
+                </tr>
+            </thead>
+            <?php
+            $orders_stmt = "SELECT * FROM products WHERE product_id IN (SELECT product_id FROM order_info WHERE order_id IN (SELECT order_id FROM orders WHERE user_id LIKE " . $_SESSION['user_idx'] . " ))";
+            $orders = $db_con->query("SELECT * FROM products WHERE product_id IN (SELECT product_id FROM order_info WHERE order_id IN (SELECT order_id FROM orders WHERE user_id LIKE " . $_SESSION['user_idx'] . " ))");
+            while ($order = $orders->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $order['product_name'] . "</td>";
+                echo "<td>" . "<img src='" . $order['image'] . "' alt='Zdjęcie produktu'>" . "</td>";
+                echo "<td>" . "<a class='buttonAdd btn' href='" . $order['image'] . "' download>Pobierz zdjęcie</a>" . "</td>";
+                echo "</tr>";
+            }
+
+            ?>
+        </table>
+    </main>
+</div>
+<?php
+include('footer.php'); ?>
