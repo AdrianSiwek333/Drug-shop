5cf8c2f58f6da3dbc1d4cd5f6153c91b29518c02
mlonstereck <118387095+mlonstereck@users.noreply.github.com>
2022-12-11 16:49:42 +0100

    Update clients_admin.php

--- clients_admin.php
+++ clients_admin.php
@@ -2,24 +2,26 @@
 include('header.php');
 include('admin-verification.php');
 ?>
+<div class="container">
     <main>
         <style>
-            table, thead, td, th {
-                border: solid 1px black;
-            }
+       
 
             label::before {
                 visibility: hidden;
             }
+           
+           
         </style>
-        <table>
+       <div class="d-inline-flex p-1">
+        <table class="table table-bordered">
             <thead>
-            <th>user_id</th>
-            <th>email</th>
-            <th>password</th>
-            <th>user_type</th>
-            <th>Przycisk edycji</th>
-            <th>Przycisk usuń</th>
+            <th scope="col">user_id</th>
+            <th scope="col">email</th>
+            <th scope="col">password</th>
+            <th scope="col">user_type</th>
+            <th scope="col">Przycisk edycji</th>
+            <th scope="col">Przycisk usuń</th>
             </thead>
             <?php
             $stmt = $db_con->query("SELECT * FROM users");
@@ -34,16 +36,16 @@ include('admin-verification.php');
                     <input type='hidden' name='email' value='" . $row['email'] . "'>
                     <input type='hidden' name='password' value='" . $row['password'] . "'>
                     <input type='hidden' name='user_type' value='" . $row['user_type'] . "'>
-                    <button name='edit' type='Submit'>Edytuj użytkownika</button>
+                    <button name='edit' class='buttonedit' type='Submit'>Edytuj użytkownika</button>
                 </form>
                 </td>";
-                echo "<td><form method='post' action='" . $_SERVER['PHP_SELF'] . "'><input type='hidden' name='user_id' value='" . $row['user_id'] . "'><button name='del' type='Submit'>Usuń użytkownika</button></form></td>";
+                echo "<td><form method='post' action='" . $_SERVER['PHP_SELF'] . "'><input type='hidden' name='user_id' value='" . $row['user_id'] . "'><button name='del' class='buttonClear' type='Submit'>Usuń użytkownika</button></form></td>";
                 echo "</tr>";
             }
             ?>
-        </table>
+        </table></div>
         <form method="post" action="clients_admin.php">
-            <button name="addClient" type="submit">Dodaj nowego użytkownika</button>
+            <button name="addClient" class='buttonedit' type="submit">Dodaj nowego użytkownika</button>
         </form>
         <?php
         if (isset($_POST['del'])) {
@@ -60,6 +62,7 @@ include('admin-verification.php');
             echo '<meta http-equiv="Refresh" content="0;' . $page . '">';
         }
         if (isset($_POST['edit'])) {
+            echo "<div class='howToContact'>";
             echo "<form method='post' action='client_edit_admin.php'>";
             echo "<input name='user_id_c' type='hidden' value='" . $_POST['user_id'] . "'>";
             echo "<label for='email'>E-mail</label>";
@@ -68,22 +71,23 @@ include('admin-verification.php');
             echo "<input type='text' name='password_c' id='password' value='' placeholder='Wpisz nowe hasło(zostanie ono zhashowane automatycznie)' required>";
             echo "<label for='user_type'>Typ użytkownika</label>";
             echo "<input type='text' name='user_type_c' id='user_type' value='" . $_POST['user_type'] . "' required>";
-            echo "<button name='commit' type='submit'>Zaakceptuj zmiany</button>";
+            echo "<button name='commit' type='submit' class='buttonedit'>Zaakceptuj zmiany</button>";
             echo "</form>";
         }
         if (isset($_POST['addClient'])) {
+            echo "<div class='howToContact'>";
             echo "<form method='post' action='client_add_admin.php'>";
             echo "<label for='email'>E-mail</label>";
-            echo "<input type='text' name='email_a' id='email' placeholder='E-mail' required>";
+            echo "<input type='text' name='email_a' id='email' placeholder='E-mail' required><br>";
             echo "<label for='password'>Hasło (zostanie ono zhashowane automatycznie)</label>";
-            echo "<input type='text' name='password_a' id='password' placeholder='Hasło' required>";
+            echo "<input type='text' name='password_a' id='password' placeholder='Hasło' required><br>";
             echo "<label for='user_type'>Typ użytkownika</label>";
-            echo "<input type='text' name='user_type_a' id='user_type' placeholder='Typ użytkownika'>";
-            echo "<button type='submit'>Dodaj użytkownika</button>";
+            echo "<input type='text' name='user_type_a' id='user_type' placeholder='Typ użytkownika'><br>";
+            echo "<button type='submit' class='buttonedit'>Dodaj użytkownika</button>";
             echo "</form>";
         }
         ?>
-    </main>
+    </main></div>
 <?php
 include('footer.php');
 ?>
