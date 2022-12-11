937fd50b919be5c04612fed3a63e1a09c7fee27e
AdrianSiwek333 <118385984+AdrianSiwek333@users.noreply.github.com>
2022-12-11 22:32:16 +0100

    Update favourite.php

--- favourite.php
+++ favourite.php
@@ -11,11 +11,11 @@ include('header.php'); ?>
       <div class="col-md-12 col-xl-10">
         <div class="card shadow-0 border rounded-3">
           <div class="card-body">
-            <div class="row">
+            <div class="row align-items-center">
               <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
                 <div class="bg-image hover-zoom ripple rounded ripple-surface">
                   <img src="<?=$val['image']?>"
-                    class="w-100" />
+                    class="w-100 border border-dark rounded" />
                   <a href="#!">
                     <div class="hover-overlay">
                       <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
@@ -34,27 +34,34 @@ include('header.php'); ?>
                   </div>
                 </div>
                 <div class="mt-1 mb-0 text-muted small">
-                  <span>Kategoria</span>
+                  <span>Opis produktu</span>
                 </div>
-                <p class="text-truncate mb-4 mb-md-0">
+                <p>
                  <?=$val['description']?>
                 </p>
               </div>
               <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                <div class="d-flex flex-row align-items-center mb-1">
+                <div class="d-flex flex-row align-items-center justify-content-center mb-1">
                   <h4 class="mb-1 me-1"><?=$val['price']?> $</h4>
                 </div>
-                <h6 class="text-success">Free shipping</h6>
-                <div class="d-flex flex-column mt-4">
+                <div class="row">
                     <form method="post">
                     <button type="submit" name="produkt" value="<?=$val['product_id']?>" class="buttonBlue">Sprawdź</button>
+                    <div class="row">
+                    <div class="col-2">
+                    </div>
+                      <div class="col-4 justify-content-right">
                     <a href="actions.php?action_type=add_item&product_id=<?= $val['product_id'] ?>&product_name=<?= $val['product_name'] ?>&image=<?= $val['image'] ?>&quantity=1&price=<?= $val['price'] ?>"
-                                    class="btn btn-dark btn-rounded mr-1"> <i class="bi bi-bag cart"></i></a>
+                                    class="btn btn-dark btn-rounded mr-1"> Kup</a>
+                      </div>
+                      <div class="col-4">
                                     <a type="button" href="favouritescript.php?action_type=remove_item&index=<?= $key ?>"
                             class="btn btn-danger btn-md mb-2" data-mdb-toggle="tooltip">
                                     Usuń
 
                                 </a>
+                        </div>
+                    </div>
                  </form>
                 </div>
               </div>
