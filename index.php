06ec478ddc3d56cec31cfd1b70978cbdc0241c81
mlonstereck <118387095+mlonstereck@users.noreply.github.com>
2022-12-12 19:45:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,54 @@
   <?php
     include('header.php');
     ?>
+    
 <main>
     <div class="container">
-        <div class="row">
+    <div class="row">
             <div class="col-md-12">
                 <h1 class="siteTitle">Strona Główna</h1>
             </div>
         </div>
+        
+            
+            <div class="col-md-12">
+               <div class="row bsp_row-underline">
+                  <div class="col-md-8">
+                     <span class="pull-left bsp_deal-text">Polecane Produkty</span>
+                  </div>
+               </div>
+               <div class="row">
+               <?php
+            $stmt=$db_con->query('SELECT * from products where category_id="3" LIMIT 4');
+            while($row=$stmt->fetch())
+            {
+
+?>
+                  <div class="col-lg col-md col-sm-6  bsp_padding-0">
+                     <div class="bsp_bbb_item ">
+                        <img src="<?= $row['image']?>" class="bsp_image">
+                        <h5 class="bsp_card-title"><?= $row['product_name']?></h5>
+                        <div class="text-center">
+                        <p class="bsp_card-text"><?= $row['price']?> $</p>
+                        <form method="post">
+                    <button type="submit" name="produkt" value="<?=$row['product_id']?>" class="buttonBlue">Sprawdź</button>
+                    </form>
+                      <p><?= $row['description']?></p>
+                        </div>
+                     </div>
+                     
+                  </div>
+           
+                  <?php } ?>
+            
+            </div></div>
+            
+            <div class="row bsp_row-underline">
+            <div class="col-md-8">
+                     <span class="pull-left bsp_deal-text">Ogłoszenia</span>
+                  </div>
+
+            </div>
         <div class="row">
             <div class="col-md-12">
 
@@ -23,7 +64,9 @@
                             <img src="<?= $row['image']?>" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
+
                             <div class="card-body">
+                                
                                 <h5 class="card-title"><?=$row['title']?></h5>
                                 <p class="card-text"><?=$row['text']?></p>
                             </div>
@@ -35,7 +78,10 @@
             }
             ?>
             </div>
-            <div class="siteSpliter">
+            <div class="row bsp_row-underline">
+            <div class="col-md-8">
+                     <span class="pull-left bsp_deal-text">Nasze Produkty</span>
+                  </div>
 
             </div>
             <div class="slickProduct">
